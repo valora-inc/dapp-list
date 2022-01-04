88ad1f0c9b8795b77e365908a9c2bbba42ecf84b
Silas Boyd-Wickizer <silasbw@gmail.com>
2022-01-04 13:36:46 -0800

    Tom comments

--- src/schema.ts
+++ src/schema.ts
@@ -1,12 +1,14 @@
 import Joi from 'joi'
 
+const colorCodePattern = /^#([A-Fa-f0-9]{6}|[A-Fa-f0-9]{3})$/
+
 export default Joi.object({
   categories: Joi.array().items(
     Joi.object({
       id: Joi.string(),
       name: Joi.string(),
-      backgroundColor: Joi.string(),
-      fontColor: Joi.string(),
+      backgroundColor: Joi.string().pattern(colorCodePattern),
+      fontColor: Joi.string().pattern(colorCodePattern),
     }),
   ),
   applications: Joi.array().items(
@@ -15,8 +17,8 @@ export default Joi.object({
       name: Joi.string(),
       categoryId: Joi.string(),
       description: Joi.string(),
-      logoUrl: Joi.string(),
-      url: Joi.string(),
+      logoUrl: Joi.string().uri(),
+      url: Joi.string().uri(),
     }),
   ),
 })

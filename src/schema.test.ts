d137ab359a789a952338c1905d8eb260e15cdc85
Silas Boyd-Wickizer <silasbw@gmail.com>
2022-01-05 07:57:27 -0800

    comments

--- src/schema.test.ts
+++ src/schema.test.ts
@@ -1,7 +1,7 @@
 import schema from './schema'
-const appList = require('./valora-app-list.json')
+const appList = require('./valora-dapp-list.json')
 
-describe('valora-app-list.json', () => {
+describe('valora-dapp-list.json', () => {
   it('complies with schema', () => {
     const validationResult = schema.validate(appList)
     expect(validationResult.error).toBe(undefined)

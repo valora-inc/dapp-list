c17360c2e543c2e0aa887c0ea653b056bce73b3c
Silas Boyd-Wickizer <silasbw@gmail.com>
2022-01-05 07:57:55 -0800

    tweak

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,8 +2,5 @@ module.exports = {
   extends: ['@valora/eslint-config-typescript', 'plugin:json/recommended'],
   parserOptions: {
     project: './tsconfig.json',
-  },
-  rules: {
-    'no-console': ['error', { allow: ['none'] }]
   }
 }

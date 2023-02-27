2dae7facd1914ca20536c786738280ded8a96410
Jean Regisser <jean.regisser@gmail.com>
2023-02-27 16:26:49 +0100

    feat: add description for the copy experiment

--- src/locales.test.ts
+++ src/locales.test.ts
@@ -2,7 +2,9 @@ import base from '../locales/base.json'
 import valoraDappList from './valora-dapp-list.json'
 
 describe('locales', () => {
-  const applicationIds = Object.keys(base.dapps)
+  const applicationIds = Object.keys(base.dapps).concat(
+    Object.keys(base.dappsExperiment),
+  )
 
   it.each(applicationIds)(
     '%s has an associated application entry',

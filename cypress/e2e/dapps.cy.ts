8285da39ba2372a68ddc6fe79920b6c0da404240
MuckT <mcgtom10@gmail.com>
2023-04-05 11:56:25 -0700

    style: prettier

--- cypress/e2e/dapps.cy.ts
+++ cypress/e2e/dapps.cy.ts
@@ -1,15 +1,14 @@
 /// <reference types="cypress" />
 
-
 import data from '../../src/valora-dapp-list.json'
 
 describe('Dapp Up Check', () => {
-  for(const application of data.applications) {
+  for (const application of data.applications) {
     if (application.url.startsWith('celo://')) continue
     it(`should return 200 for ${application.url}`, async () => {
       cy.request(application.url).then((response) => {
         expect(response.status).to.eq(200)
       })
     })
-  } 
+  }
 })

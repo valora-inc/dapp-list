be9516dd64b0b599da303d793a523a63af874775
MuckT <mcgtom10@gmail.com>
2023-04-05 11:53:25 -0700

    test(e2e): avoid testing celo:// urls

--- cypress/e2e/dapps.cy.ts
+++ cypress/e2e/dapps.cy.ts
@@ -5,8 +5,8 @@ import data from '../../src/valora-dapp-list.json'
 
 describe('Dapp Up Check', () => {
   for(const application of data.applications) {
+    if (application.url.startsWith('celo://')) continue
     it(`should return 200 for ${application.url}`, async () => {
-      if (application.url.startsWith('celo://')) return
       cy.request(application.url).then((response) => {
         expect(response.status).to.eq(200)
       })

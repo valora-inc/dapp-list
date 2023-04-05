020ee94c254a1389a9097575754e02afcf204497
MuckT <mcgtom10@gmail.com>
2023-04-05 08:49:35 -0700

    style: prettier

--- src/dapps.e2e.ts
+++ src/dapps.e2e.ts
@@ -9,9 +9,7 @@ describe('Deployed URL check', () => {
         // Skip celo:// urls
         if (application.url.startsWith('celo://')) return
         // Allow one redirect
-        const response = await request(application.url)
-          .get('')
-          .redirects(1)
+        const response = await request(application.url).get('').redirects(1)
         expect(response.status).toBe(200)
       },
       15 * 1000,

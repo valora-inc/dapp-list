1b2c119ebe28d1dd80c451eaeb398b9eb46cfe52
MuckT <mcgtom10@gmail.com>
2023-04-04 19:18:40 -0700

    test(e2e): add a deployed url check for dapps

---
+++ src/dapps.e2e.ts
@@ -0,0 +1,13 @@
+import data from './valora-dapp-list.json'
+import request from 'supertest'
+
+describe('Deployed URL check', () => {
+  for (const application of data.applications) {
+    it(`should return 200 for ${application.url}`, async () => {
+      // Skip celo:// urls
+      if (application.url.startsWith('celo://')) return
+      const response = await request(application.url).get('')
+      expect(response.status).toBe(200)
+    })
+  }
+})

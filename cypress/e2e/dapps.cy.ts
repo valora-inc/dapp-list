20da1a9ec462ecf4f4213f18fca845b3fb3e158c
MuckT <mcgtom10@gmail.com>
2023-04-05 12:07:02 -0700

    test(e2e): remove async from cypress test

--- cypress/e2e/dapps.cy.ts
+++ cypress/e2e/dapps.cy.ts
@@ -5,7 +5,7 @@ import data from '../../src/valora-dapp-list.json'
 describe('Dapp Up Check', () => {
   for (const application of data.applications) {
     if (application.url.startsWith('celo://')) continue
-    it(`should return 200 for ${application.url}`, async () => {
+    it(`should return 200 for ${application.url}`, () => {
       cy.request(application.url).then((response) => {
         expect(response.status).to.eq(200)
       })

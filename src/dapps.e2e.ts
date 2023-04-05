d9e6c5621aa29d4ad2a15b30a278df07c1109034
MuckT <mcgtom10@gmail.com>
2023-04-05 00:22:24 -0700

    style: prettier

--- src/dapps.e2e.ts
+++ src/dapps.e2e.ts
@@ -3,11 +3,15 @@ import request from 'supertest'
 
 describe('Deployed URL check', () => {
   for (const application of data.applications) {
-    it(`should return 200 for ${application.url}`, async () => {
-      // Skip celo:// urls
-      if (application.url.startsWith('celo://')) return
-      const response = await request(application.url).get('')
-      expect(response.status).toBe(200)
-    }, 15 * 1000)
+    it(
+      `should return 200 for ${application.url}`,
+      async () => {
+        // Skip celo:// urls
+        if (application.url.startsWith('celo://')) return
+        const response = await request(application.url).get('')
+        expect(response.status).toBe(200)
+      },
+      15 * 1000,
+    )
   }
 })

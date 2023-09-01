9dd70c857e5055ae04cf303148e75784eadbf4d9
Jean Regisser <jean.regisser@gmail.com>
2023-09-01 11:05:43 +0200

    Address warning

--- jest.config.js
+++ jest.config.js
@@ -2,12 +2,7 @@ module.exports = {
   watchman: false,
   moduleFileExtensions: ['ts', 'js', 'json'],
   transform: {
-    '^.+\\.(ts)$': 'ts-jest',
+    '^.+\\.(ts|tsx)$': ['ts-jest', { tsconfig: 'tsconfig.json' }],
   },
   testPathIgnorePatterns: ['dist'],
-  globals: {
-    'ts-jest': {
-      tsconfig: 'tsconfig.json',
-    },
-  },
 }
